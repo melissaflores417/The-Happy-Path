cda5c43d1a88f2432f4c7e48f37d2dd25a28dd03
melissaflores417 <melissaflores417@nevada.unr.edu>
2022-04-30 19:11:13 -0700

    second commit

--- happypathapp/App.js
+++ happypathapp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Happy Path</Text>
+      <Text>Happy Path </Text>
       <StatusBar style="auto" />
     </View>
   );
