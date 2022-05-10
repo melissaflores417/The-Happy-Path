8982fdad3f08daf39858d11365aa1a20ea3d4943
Thaddaeus Lam <thad.h.lam@gmail.com>
2022-05-10 04:32:10 -0700

    fixed tips content

--- happypathapp/Screens/Tips.js
+++ happypathapp/Screens/Tips.js
@@ -10,8 +10,6 @@ function Tips() {
         <Text style = {{fontSize:'20', fontWeight: 'bold', color:'#2c29b2', fontFamily: 'Avenir'}}>Improve your daily routine</Text>
 
         <Text style = {{textAlign: 'center', fontSize:'16', fontWeight: 'regular', color:'#252525', fontFamily: 'Avenir', marginTop: 10}}>Here is a selection of activites {'\n'} and tips you can choose from.</Text>
-
-        <Text style = {{ textAlign: 'center', fontSize:'16', fontWeight: 'regular', color:'#252525', fontFamily: 'Avenir', marginTop: 10}}>Here is a selection of activites {'\n'} and tips you can choose from.</Text>
         
         <Text style ={{marginTop: 10, fontWeight: 'bold', fontFamily: 'Avenir'}}>What would you like to focus on today?</Text>
 
